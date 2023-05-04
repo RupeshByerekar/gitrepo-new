b7fc02ea36dc1a22070060dddb1bdd05af30323f
Rupesh <mryerekar2020@gmail.com>
2023-05-04 16:47:53 +0530

    adding msg to Demo1.java

--- spring_boot/src/spring_boot/Demo1.java
+++ spring_boot/src/spring_boot/Demo1.java
@@ -2,6 +2,7 @@ package spring_boot;
 
 public class Demo1 {
 public static void main(String[] args) {
-	System.out.println("this is demo1");
+	System.out.println("this is demo1 nice to meet you");
 }
 }
+ 
